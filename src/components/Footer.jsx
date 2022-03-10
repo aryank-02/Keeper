643df778a9333f21914fbbab3b552b876abf5a95
aryank-02 <aaryan.kekre22gmail.com>
2022-03-10 22:04:40 +0530

    init commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer () {
+
+const year = new Date;
+const currentyear = year.getFullYear();
+
+return (<footer><p>Copyright{currentyear}</p>
+</footer>)
+}
+
+export default Footer;
